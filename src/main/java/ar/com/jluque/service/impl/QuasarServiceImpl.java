eaee4769dfa98083f2e8e435b2dbc24a2be2e992
Julio <44620800+julioluque@users.noreply.github.com>
2023-08-02 19:06:09 -0300

    refactor splited and prepare builder message

--- src/main/java/ar/com/jluque/service/impl/QuasarServiceImpl.java
+++ src/main/java/ar/com/jluque/service/impl/QuasarServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.Spanish;
 import org.languagetool.rules.RuleMatch;
-import org.languagetool.synthesis.SynthesizerTools;
 import org.springframework.stereotype.Service;
 
 import ar.com.jluque.dto.PositionDto;
@@ -22,8 +21,10 @@ import ar.com.jluque.mapper.QuasarMapper;
 import ar.com.jluque.mapper.TriangulateMapper;
 import ar.com.jluque.mapper.ValidateMapper;
 import ar.com.jluque.service.QuasarService;
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class QuasarServiceImpl implements QuasarService {
 
 	@Override
@@ -35,8 +36,9 @@ public class QuasarServiceImpl implements QuasarService {
 
 		Point posicionesLibreria = TriangulateMapper.triangulacionLibrary(positions, distances);
 		Point posicionesFormula = TriangulateMapper.triangularFormula(positions, distances);
-		System.out.println(posicionesLibreria);
-		System.out.println(posicionesFormula);
+		log.info("posicionesUsandoLibreria: {} ", posicionesLibreria);
+		log.info("posicionesUsandoformula: {} ", posicionesFormula);
+
 		return posicionesFormula;
 	}
 
@@ -44,101 +46,49 @@ public class QuasarServiceImpl implements QuasarService {
 	public String getMessage(String[][] messages) {
 		ValidateMapper.amountMessages(messages);
 
-		System.out.println(">>>>>>>>>COMPARAMOS validateWord<<<<<<<<<");
-		validateWord(messages);
-
-		System.out.println(">>>>>>>>>COMPARAMOS validateWord2<<<<<<<<<");
-		validateWord2(messages);
-
-		return "Este es el mensaje";
+		List<List<String>> splitedMessages = validateAndSplitWords(messages);
+		return buildMessage(splitedMessages);
 	}
 
-	private void validateWord(String[][] wordMatrix) {
+	private List<List<String>> validateAndSplitWords(String[][] wordMatrix) {
 		JLanguageTool languageTool = new JLanguageTool(new Spanish());
+		List<List<String>> mensajeOriginalList = Arrays.stream(wordMatrix).map(Arrays::asList)
+				.collect(Collectors.toList());
 
-		List<List<String>> palabrasList = Arrays.stream(wordMatrix).map(Arrays::asList).toList();
+		List<List<String>> mensajeModificadoList = new ArrayList<>();
 
-		try {
+		mensajeOriginalList.stream().forEach(palabras -> {
+			List<String> palabrasModificadas = new ArrayList<>();
 
-			palabrasList.stream().flatMap(List::stream).forEach(palabra -> {
+			palabras.forEach(palabra -> {
 				try {
 					List<RuleMatch> matches = languageTool.check(palabra);
 
 					if (matches.isEmpty()) {
-						System.out.println("La palabra '" + palabra + "' es válida.");
+						log.info("[{}]\t es valida", palabra);
 					} else {
-						System.out.print("La palabra '" + palabra
-								+ "' es inexistente o incorrecta. y la reemplazamos con espacio");
+						log.info("[{}]\t no reconocida. La reemplazamos con espacio", palabra);
 						palabra = "";
-						System.out.println(" -->  nueva palabra [" + palabra + "agregartexto]");
 
 					}
+					palabrasModificadas.add(palabra);
+
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-
 			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void validateWord2(String[][] wordMatrix) {
-		JLanguageTool languageTool = new JLanguageTool(new Spanish());
-		List<List<String>> palabrasList = Arrays.stream(wordMatrix).map(Arrays::asList).collect(Collectors.toList());
-
-		List<List<String>> palabrasListModificada = new ArrayList<>();
-		try {
-
-			palabrasList.stream().forEach(palabras -> {
-				List<String> palabrasModificadas = new ArrayList<>();
-
-				palabras.forEach(palabra -> {
-					try {
-						List<RuleMatch> matches = languageTool.check(palabra);
-
-						if (matches.isEmpty()) {
-							System.out.println("La palabra '" + palabra + "' es válida.");
-//                            palabrasModificadas.add(palabra);
-						} else {
-							System.out.print("La palabra '" + palabra
-									+ "' es inexistente o incorrecta. y la reemplazamos con espacio");
-							palabra = "";
-							System.out.println(" -->  nueva palabra [" + palabra + "agregartexto]");
-
-						}
-						palabrasModificadas.add(palabra);
-
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				});
-				palabrasListModificada.add(palabrasModificadas);
-			});
-
-//            palabrasList = palabrasListModificada;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("---1----");
-		palabrasList.forEach(palabras -> {
-			palabras.forEach(palabra -> {
-				System.out.println("La palabra modificada es: " + palabra);
-			});
-		});
-		palabrasListModificada.forEach(palabras -> {
-			palabras.forEach(palabra -> {
-				System.out.println("La palabra modificada es: " + palabra);
-			});
+			mensajeModificadoList.add(palabrasModificadas);
 		});
 
-		System.out.println("---2----");
-		System.out.println(palabrasList);
-		System.out.println(palabrasListModificada);
-
+		log.info("compare Original: {}\ncompare Updated: {}", mensajeOriginalList, mensajeModificadoList);
+		return mensajeModificadoList;
 	}
 
+	private String buildMessage(List<List<String>> messages) {
+		//TODO buildMessage
+		return messages.toString();
+	}
+	
 	/**
 	 * NIVEL 2
 	 */

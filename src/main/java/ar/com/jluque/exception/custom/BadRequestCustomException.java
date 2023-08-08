3b1fc018f4d54a52b841f9fe6df98aff0e4e1810
Julio <44620800+julioluque@users.noreply.github.com>
2023-08-08 09:18:57 -0300

    completando libreria de exepciones

---
+++ src/main/java/ar/com/jluque/exception/custom/BadRequestCustomException.java
@@ -0,0 +1,12 @@
+package ar.com.jluque.exception.custom;
+
+public class BadRequestCustomException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String DESCRIPTION = "Bad Request Exception";
+
+	public BadRequestCustomException(String detail) {
+		super(DESCRIPTION + ". " + detail);
+	}
+}
---
+++ src/main/java/ar/com/jluque/exception/custom/DataIntegrityCustomException.java
@@ -0,0 +1,13 @@
+package ar.com.jluque.exception.custom;
+
+public class DataIntegrityCustomException extends ConflictCustomException {
+	
+	private static final long serialVersionUID = 1L;
+
+	private static final String DESCRIPTION = "Data Integrity Violation Exception";
+
+	public DataIntegrityCustomException(String detail) {
+		super(DESCRIPTION + ". " + detail);
+	}
+
+}
